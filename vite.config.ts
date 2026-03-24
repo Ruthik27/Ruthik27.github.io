623800d135374bd0851bdae166a3fbdf6cc83517
Ruthik Kale <62241739+Ruthik27@users.noreply.github.com>
2026-03-24 15:28:31 -0400

    added deploy and config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
